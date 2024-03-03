46be605b0c9023b9fd2d82ca0fef35b3e68c2a09
pratikkhandake <160304215+pratikkhandake@users.noreply.github.com>
2024-03-03 16:47:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,4 +44,4 @@ def predict():
     return render_template('index.html', prediction=result)
 
 if __name__ == '__main__':
-    app.run(host=0.0.0.0,debug=False)
+    app.run(host='0.0.0.0', port=8081,debug=False)
